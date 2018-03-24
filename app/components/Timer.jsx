1d29ed5d3bca1c97dac509023d015e9b1d9982b6
Saurabh Sood <saurabhsood91@gmail.com>
2018-03-24 12:52:02 -0700

    minor change to rendering of time

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -15,6 +15,14 @@ class Timer extends React.Component {
         let timeInSeconds = seconds % 60;
         let minutes = Math.floor(seconds / 60);
 
+        if(timeInSeconds < 10) {
+            timeInSeconds = '0' + timeInSeconds;
+        }
+
+        if(minutes < 10) {
+            minutes = '0' + minutes;
+        }
+
         return `${minutes}:${timeInSeconds}`;
     }
 
